9d5d075f9afd6bdad0f88b66861a7e4db2ef0fb5
acrou <acrouch@mymail.mines.edu>
2015-01-20 16:21:37 -0700

    beginning on calculator

--- ViewController.m
+++ ViewController.m
@@ -2,14 +2,17 @@
 //  ViewController.m
 //  Calculator
 //
-//  Created by  Allison Crouch on 1/20/15.
-//  Copyright (c) 2015 CSCI448 Allison Crouch. All rights reserved.
+//  Created by 
+Allison Crouch on 1/20/15.
+//  Copyright (c) 2015 CSCI448
+Allison Crouch. All rights reserved.
 //
 
 #import "ViewController.h"
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *display;
 @end
 
 @implementation ViewController
