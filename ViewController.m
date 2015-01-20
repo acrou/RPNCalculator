d5a6091d28c87ac9a47733c7a57d99324031832c
acrou <acrouch@mymail.mines.edu>
2015-01-20 16:05:37 -0700

    Initialized project

---
+++ ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Calculator
+//
+//  Created by  Allison Crouch on 1/20/15.
+//  Copyright (c) 2015 CSCI448 Allison Crouch. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
